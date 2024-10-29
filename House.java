ce935b373cd77e1d6eb27a993af99beb7848cdf7
pwestland <pwestland@smith.edu>
2024-10-29 19:47:47 -0400

    House initial

--- House.java
+++ House.java
@@ -1,8 +1,58 @@
-/* This is a stub for the House class */
-public class House {
 
-  public House() {
+import java.util.ArrayList;
+
+/* This is a stub for the House class */
+public class House extends Building {
+  
+  private ArrayList<String> residents;
+  private boolean hasDiningHall;
+
+  /* creates the house */
+
+  public House(String name, String address, int nFloors, boolean hasDiningHall) {
+    super(name, address, nFloors);
+    this.residents = new ArrayList<>();
+    this.hasDiningHall = hasDiningHall;
+    
     System.out.println("You have built a house: 🏠");
+  }
+  
+  /* returns whether house has a dining hall */
+
+  public boolean hasDiningHall(){
+    return this.hasDiningHall;
+  }
+
+  /* returns how many residents in the house */
+
+  public int nResidents(){
+    return this.residents.size();
+  }
+
+  /* moves a person into the house */
+
+  public void moveIn(String name){
+    if (this.residents.contains(name)){
+      throw new RuntimeException(name + " already lives in " + this.name);
+    }
+    this.residents.add(name);
+
+  }
+
+  /* moves a person out of the house, as long as they already live there */
+
+  public String moveOut(String name){
+    if (this.residents.contains(name)){
+      this.residents.remove(name);
+      return(name + " no longer lives in " + this.name);
+    }
+    else{
+      throw new RuntimeException(name + " does not live in " + this.name);
+    }
+  }
+
+  public boolean isResident(String person){
+    return this.residents.contains(person);
   }
 
   public static void main(String[] args) {
